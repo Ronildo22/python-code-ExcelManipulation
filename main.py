5a3fcf8771b27ac7cb6382d2ea414b1d35a9dcdc
Ronildo Santos <85889292+Ronildo22@users.noreply.github.com>
2023-01-03 15:13:05 -0300

    Update py 03/01

--- main.py
+++ main.py
@@ -1,50 +1,179 @@
-
 from openpyxl import Workbook, load_workbook
+import pandas as pd
 
-
-
-planilha = load_workbook("Text.xlsx")
+planilha = load_workbook("Text.xlsx", data_only = True)
 
 aba_ativa = planilha.active
-# I
-for celula in aba_ativa["E"]:
-     if celula.value == 35:
-        linha = celula.row
-        aba_ativa[f"E{linha}"] = " "
+
+
+ws = planilha['my test']
+
+
+sheet_obj = planilha.active
+row = sheet_obj.max_row
+column = sheet_obj.max_column
+
+album = {'nome':'A Night at the Opera,' ' TESTE1,' ' TESTE2',' artista':'Blind Guardian','lançamento':2002}
+
+
+# df = pd.read_excel("", sheet_name="Planilha2")
+
+
+
+
+
+
+#Consultando a tabela toda
+# df = pd.read_excel("Text.xlsx")
+# print(df)
+
+
+
+#SELECIONANDO UMA PLANILHA COM PANDAS
+                              # *sheet_name* Escolhe planilha dentro do arq
+# df2 = pd.read_excel("Text.xlsx", sheet_name=1, usecols=None)
+# print(df2)
+
+
+
+#SELECIONANDO UMA COLUNA COM PANDAS
+# df2 = pd.read_excel("Text.xlsx", sheet_name=1, usecols="A")
+# print(df2)
+
+
+
+#SELECIONADO UMA COLUNA PULANDO linhas  COM PANDAS
+# df3 = pd.read_excel("Text.xlsx", sheet_name=1,skiprows=2, usecols="A")
+# print(df3)
+
+
+
+#SELECIONADO UMA LINHA COM PANDAS
+df4 = pd.read_excel("Text.xlsx", sheet_name=1,nrows=1)
+print(df4)
+
+
+
+#ws['G1'] = '=SUM(F1:F13)'
+#ws['G3'] = '=AVERAGE(F1:F13)'
+
+
+
+# for i in range(1, row + 1):
+#       cell_obj = sheet_obj.cell(row = i, column = 7)
+#       print(cell_obj.value)
+
+
+
+# for celula in aba_ativa["G"]:
+#       if celula.value == "oi":
+#          linha = celula.row
+#          aba_ativa[f"G{linha}"] = ""
+
 
 planilha.save("Text.xlsx")
 
 
 
-#
-#
+# teste = ws['G1'].value
+#print(teste)
+
+
+
+#print(teste)
+#print(diciTeste)
+
+
+#calculando a Media
+# df = pd.DataFrame(diciTeste)
+# mean_df = df['dados'].mean()
+# print(mean_df)
+
+#PERCORRENDO td um dicionario
+# for key in album:
+#    print(album[key])
+
+
+#print(album['nome'])
+list = []
+
+#list.append(album['nome'])
+
+
+#print(list)
+
+
+#Inserção de dados simples
+#ws['E1'] = "inserção"
+
+
+
+#Consulta de dados simples
+teste = ws['E1'].value
+#print(teste)
+
+
+
+
+
+
+# for i in range(1, row + 1):
+#      cell_obj = sheet_obj.cell(row = i, column = 5)
+#      print(cell_obj.value)
+
+
+
+# INSERINDO DADOS COMPARANDO
+
+# for celula in aba_ativa["I"]:
+#      if celula.value == "oi":
+#         linha = celula.row
+#         aba_ativa[f"E{linha}"] = "uu"
+
+print(f'\n')
+
+#CONSULTADO DADOS  Quant de Dados
+# index= 0
+# for celula in aba_ativa["F"]:
+#      if celula.value == 600:
+#             index +=1
+# print(index)
+
+
+
+#album = {'nome':'A Night at the Opera','artista':'Blind Guardian','lançamento':2002}
+
+
+#função range()
+
+# teste = range(1, 10, +2)
+# print(f'\n')
+# for x in teste:
+#     print(x)
+
+
+
+
+
+
 # sheet_obj = planilha.active
-#
+
+
 # row = sheet_obj.max_row
 # column = sheet_obj.max_column
-#
+
+
 # print("Total Rows:", row)
 # print("Total Columns:", column)
-#
-# # SELECIONADO PRIMEIRA COLUNA
+
+
+#  SELECIONADO  COLUNA
 # for i in range(1, row + 1):
 #     cell_obj = sheet_obj.cell(row = i, column = 1)
 #     print(cell_obj.value)
-#
-# # SELECIONANDO PRIMEIRA LINHA
+
+
+# # SELECIONANDO  LINHA
 # for i in range(1, column + 1):
 #     cell_obj = sheet_obj.cell(row = 1, column = i)
 #     print(cell_obj.value, end = " | ")
-
-
-
-# LINKS USADOS
-# https://acervolima.com/trabalho-com-planilhas-do-excel-em-python/
-# https://www.hashtagtreinamentos.com/integrar-excel-com-python
-
-
-
-
-
-
-
